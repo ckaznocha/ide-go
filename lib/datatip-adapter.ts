34602ab111e720ec1f893d16b2cadce1e7e6c603
Clifton Kaznocha <ckaznocha@gmail.com>
2018-08-26 13:02:52 -0700

    Update atom-languageclient for new utils module and import only required function (#33)

--- lib/datatip-adapter.ts
+++ lib/datatip-adapter.ts
@@ -2,7 +2,7 @@ import { Point, TextEditor } from 'atom';
 import { Datatip } from 'atom-ide';
 import DatatipAdapter from 'atom-languageclient/build/lib/adapters/datatip-adapter';
 import { LanguageClientConnection } from 'atom-languageclient/build/lib/languageclient';
-import * as Utils from 'atom-languageclient/build/lib/utils';
+import { getWordAtPosition } from 'atom-languageclient/build/lib/utils';
 
 export class GoDatatipAddapter extends DatatipAdapter {
     async getDatatip(
@@ -12,7 +12,7 @@ export class GoDatatipAddapter extends DatatipAdapter {
     ): Promise<Datatip | null> {
         let res = await super.getDatatip(connection, editor, point)
         if (res) {
-            res.range = Utils.getWordAtPosition(editor, point)
+            res.range = getWordAtPosition(editor, point)
         }
         return res
     }
