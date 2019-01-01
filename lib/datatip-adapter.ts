321e81d87bc6fb7f6553248bb76876196743eb9c
ckaznocha <ckaznocha@gmail.com>
2019-01-01 23:12:42 +0000

    Set up linter and formatter, fix all reported issues

--- lib/datatip-adapter.ts
+++ lib/datatip-adapter.ts
@@ -1,16 +1,18 @@
-import { Point, TextEditor } from 'atom';
-import { Datatip } from 'atom-ide';
-import DatatipAdapter from 'atom-languageclient/build/lib/adapters/datatip-adapter';
-import { LanguageClientConnection } from 'atom-languageclient/build/lib/languageclient';
-import { getWordAtPosition } from 'atom-languageclient/build/lib/utils';
+import { Point, TextEditor } from 'atom'
+import { Datatip } from 'atom-ide'
+import DatatipAdapter from 'atom-languageclient/build/lib/adapters/datatip-adapter'
+import { LanguageClientConnection } from 'atom-languageclient/build/lib/languageclient'
+import { getWordAtPosition } from 'atom-languageclient/build/lib/utils'
 
-export class GoDatatipAddapter extends DatatipAdapter {
-    async getDatatip(
+/** Extends the default DatatipAdapter to work with the non-standard output. */
+export class GoDatatipAdapter extends DatatipAdapter {
+    /** Override getDatatip to correct the range. */
+    public async getDatatip(
         connection: LanguageClientConnection,
         editor: TextEditor,
         point: Point
     ): Promise<Datatip | null> {
-        let res = await super.getDatatip(connection, editor, point)
+        const res = await super.getDatatip(connection, editor, point)
         if (res) {
             res.range = getWordAtPosition(editor, point)
         }
--- lib/language-client.ts
+++ lib/language-client.ts
@@ -12,34 +12,35 @@ import { GoPlus } from '../typings/go-plus'
 import {
     AtomPluginSettings,
     getPluginSettingValue,
-    getProcessArgs
+    getProcessArgs,
 } from './atom-config'
 import {
     findOrInstallGoLangserver,
     promptToInstallGoPlusIfNeeded,
     promptToUpdateWithGoPlus,
     promptToUpgradeManually,
-    shouldUpgrade
+    shouldUpgrade,
 } from './go-env'
 
 interface InitializationOptions {
+    formatTool?: string
     funcSnippetEnabled?: boolean
     gocodeCompletionEnabled?: boolean
-    formatTool?: string
     goimportsLocalPrefix?: string
     maxParallelism?: number
     useBinaryPkgCache?: boolean
 }
 
 const GO_READY_EVENT = Symbol('ide-go-env-ready')
-const BUSY_SIGNAL_READY_EVENT = Symbol('ide-go-busy-siganl-ready')
+const BUSY_SIGNAL_READY_EVENT = Symbol('ide-go-busy-signal-ready')
 
+/** Language client for go-langserver. */
 export class GoLanguageClient extends AutoLanguageClient {
-    readonly config: AtomPluginSettings = pkg.packageSettings
-    private goGet?: GoPlus.GoGet
+    public readonly config: AtomPluginSettings = pkg.packageSettings
     private goConfig?: GoPlus.GoConfig
+    private goGet?: GoPlus.GoGet
 
-    constructor(
+    public constructor(
         private readonly emitter: EventEmitter,
         datatip: DatatipAdapter
     ) {
@@ -47,94 +48,134 @@ export class GoLanguageClient extends AutoLanguageClient {
         this.datatip = datatip
     }
 
-    getGrammarScopes(): string[] {
-        return pkg.enhancedScopes
-    }
-    getLanguageName(): string {
-        return 'Go'
-    }
-    getServerName(): string {
-        return 'go-langserver'
+    /** Loads a BusySignalService. */
+    public consumeBusySignal(service: BusySignalService): Disposable {
+        const disposable = super.consumeBusySignal(service)
+        this.emitter.emit(BUSY_SIGNAL_READY_EVENT)
+        return disposable
     }
 
-    getRootConfigurationKey(): string {
-        return pkg.name
-    }
-
-    getInitializeParams(
-        projectPath: string,
-        process: LanguageServerProcess
-    ): InitializeParams {
-        const params = super.getInitializeParams(projectPath, process)
-        params.initializationOptions = {
-            funcSnippetEnabled: !!getPluginSettingValue('funcSnippetEnabled'),
-            gocodeCompletionEnabled: !!getPluginSettingValue(
-                'completionEnabled'
-            ),
-            formatTool: getPluginSettingValue('formatTool'),
-            goimportsLocalPrefix: getPluginSettingValue('goimportsLocalPrefix'),
-            useBinaryPkgCache: !!getPluginSettingValue('useBinaryPkgCache')
-        } as InitializationOptions
-        return params
-    }
-
-    mapConfigurationObject(_configuration: any): any {
-        this.restartAllServers()
-    }
-
-    async startServerProcess(): Promise<ChildProcess> {
-        const childProcess = spawn(await this.serverPath(), getProcessArgs(), {
-            cwd: join(__dirname, '..'),
-            env: process.env
-        })
-        childProcess.on('exit', this.onExit.bind(this))
-        return childProcess
-    }
-
-    consumeGoGet(goGet: GoPlus.GoGet) {
-        this.goGet = goGet
-        if (this.goConfig) {
-            this.emitter.emit(GO_READY_EVENT)
-        }
-    }
-
-    consumeGoConfig(goConfig: GoPlus.GoConfig) {
+    /** Loads a GoPlus.GoConfig. */
+    public consumeGoConfig(goConfig: GoPlus.GoConfig): void {
         this.goConfig = goConfig
         if (this.goGet) {
             this.emitter.emit(GO_READY_EVENT)
         }
     }
 
-    private goReady(): Promise<void> {
-        return new Promise(resolve => {
-            if (this.goConfig && this.goGet) {
-                return resolve()
-            }
-            this.emitter.on(GO_READY_EVENT, resolve)
+    /** Loads a GoPlus.GoGet. */
+    public consumeGoGet(goGet: GoPlus.GoGet): void {
+        this.goGet = goGet
+        if (this.goConfig) {
+            this.emitter.emit(GO_READY_EVENT)
+        }
+    }
+
+    /** Activated for these scopes. */
+    public getGrammarScopes(): string[] {
+        return pkg.enhancedScopes
+    }
+
+    /** Initialize Params to use for the lsp. */
+    public getInitializeParams(
+        projectPath: string,
+        process: LanguageServerProcess
+    ): InitializeParams {
+        const params = super.getInitializeParams(projectPath, process)
+        const initializationOptions: InitializationOptions = {
+            formatTool: getPluginSettingValue('formatTool'),
+            funcSnippetEnabled: !!getPluginSettingValue('funcSnippetEnabled'),
+            gocodeCompletionEnabled: !!getPluginSettingValue(
+                'completionEnabled'
+            ),
+            goimportsLocalPrefix: getPluginSettingValue('goimportsLocalPrefix'),
+            useBinaryPkgCache: !!getPluginSettingValue('useBinaryPkgCache'),
+        }
+
+        params.initializationOptions = initializationOptions
+        return params
+    }
+
+    /** The programming language name. */
+    public getLanguageName(): string {
+        return 'Go'
+    }
+
+    /** They key to use for configurations. */
+    public getRootConfigurationKey(): string {
+        return pkg.name
+    }
+
+    /** The name of the lsp. */
+    public getServerName(): string {
+        return 'go-langserver'
+    }
+
+    /**
+     * Map the configuration object. Used to restart the server when the
+     * configuration changes.
+     */
+    public mapConfigurationObject(): void {
+        this.restartAllServers().catch((e: Error) => {
+            throw e
         })
     }
 
-    consumeBusySignal(service: BusySignalService): Disposable {
-        const disposable = super.consumeBusySignal(service)
-        this.emitter.emit(BUSY_SIGNAL_READY_EVENT)
-        return disposable
+    /** Starts the lsp process. */
+    public async startServerProcess(): Promise<ChildProcess> {
+        const childProcess = spawn(await this.serverPath(), getProcessArgs(), {
+            cwd: join(__dirname, '..'),
+            env: process.env,
+        })
+        childProcess.on('exit', this.onExit.bind(this))
+        return childProcess
     }
 
-    private busySignalReady(): Promise<void> {
-        return new Promise(resolve => {
-            if (this.busySignalService) {
-                return resolve()
+    private async busySignalReady(): Promise<void> {
+        return new Promise(
+            (resolve: () => void): void => {
+                if (this.busySignalService) {
+                    resolve()
+                    return
+                }
+                this.emitter.on(BUSY_SIGNAL_READY_EVENT, resolve)
             }
-            this.emitter.on(BUSY_SIGNAL_READY_EVENT, resolve)
-        })
+        )
+    }
+
+    private async goReady(): Promise<void> {
+        return new Promise(
+            (resolve: () => void): void => {
+                if (this.goConfig && this.goGet) {
+                    resolve()
+                    return
+                }
+                this.emitter.on(GO_READY_EVENT, resolve)
+            }
+        )
+    }
+
+    private onExit(code: number, _signal: string): void {
+        if (code) {
+            atom.notifications.addError(
+                `${this.getLanguageName()} language server stopped unexpectedly.`,
+                {
+                    description: `Exit code: ${code}\n${this.processStdErr}`,
+                    dismissable: true,
+                }
+            )
+        }
     }
 
     private async serverPath(): Promise<string> {
         await this.busySignalReady()
+        if (!this.busySignalService) {
+            throw new Error('Busy Signal Service not loaded.')
+        }
         const busy = this.busySignalService.reportBusy(
             `Looking for ${this.getServerName()}`
         )
-        let customPath = getPluginSettingValue('customServerPath')
+        const customPath = getPluginSettingValue('customServerPath')
         if (customPath !== this.config.customServerPath.default) {
             if (shouldUpgrade(customPath)) {
                 promptToUpgradeManually()
@@ -143,7 +184,13 @@ export class GoLanguageClient extends AutoLanguageClient {
             return customPath
         }
 
-        promptToInstallGoPlusIfNeeded(pkg.name, this.getServerName(), busy)
+        promptToInstallGoPlusIfNeeded(
+            pkg.name,
+            this.getServerName(),
+            busy
+        ).catch((e: Error) => {
+            throw e
+        })
 
         busy.setTitle(`Waiting for Go env`)
         await this.goReady()
@@ -151,7 +198,7 @@ export class GoLanguageClient extends AutoLanguageClient {
             throw new Error('Failed to load Go environment.')
         }
 
-        let serverPath = await findOrInstallGoLangserver(
+        const serverPath = await findOrInstallGoLangserver(
             pkg.name,
             this.getServerName(),
             this.goConfig,
@@ -159,7 +206,7 @@ export class GoLanguageClient extends AutoLanguageClient {
             busy
         )
 
-        if (!serverPath) {
+        if (serverPath === false) {
             throw new Error('Failed to locate language server.')
         }
         if (shouldUpgrade(serverPath)) {
@@ -170,17 +217,5 @@ export class GoLanguageClient extends AutoLanguageClient {
             )
         }
         return serverPath
-    }
-
-    private onExit(code: number, _signal: string) {
-        if (code) {
-            atom.notifications.addError(
-                `${this.getLanguageName()} language server stopped unexpectedly.`,
-                {
-                    dismissable: true,
-                    description: `Exit code: ${code}\n${this.processStdErr}`
-                }
-            )
-        }
     }
 }
